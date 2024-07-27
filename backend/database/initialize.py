ec858495dd4bbe86ea2971f9dc3a7b2876fa29ec
gegeardiyansyah <pmmb_gegeardiyansyah@pusri.co.id>
2024-07-27 22:11:39 +0700

    Update router and update collection into all route itself

--- backend/database/initialize.py
+++ backend/database/initialize.py
@@ -4,9 +4,4 @@ from pymongo import MongoClient, GEOSPHERE
 
 client = MongoClient("mongodb://localhost:27017")
 db = client['geo_database']
-collection = db['geo_collection']
-
-# Ensure the collection has a 2dsphere index
-collection.create_index([("geometry", GEOSPHERE)])
-  
 
--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from route.polygons import polygons_router
+from route.markers import markers_router
 import uvicorn
 
 app = FastAPI()
 
 app.include_router(polygons_router, prefix="/api")
+app.include_router(markers_router, prefix="/api")
 
 if __name__ == '__main__':
     uvicorn.run(app, host="127.0.0.1", port=8000)
